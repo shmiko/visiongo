89cf89f054c6682350f27a8ad81c9dbaf2f678f2
Paul Jones <shmiko@gmail.com>
2015-03-24 22:28:28 +1100

    multivision repackaged 2d

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://elite-schedule.azure-mobile.net/tables/leagues");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  next();
-});
+
 
 var config = require('./server/config/config')[env];
 
@@ -20,6 +15,13 @@ require('./server/config/mongoose')(config);
 require('./server/config/passport')();
 
 require('./server/config/routes')(app);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://elite-schedule.azure-mobile.net/tables/leagues");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  next();
+});
 
 app.listen(config.port);
 console.log('Listening on port ' + config.port + '...');
