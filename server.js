6db320d3840644f8dc7cb9fbf7812272f031163e
Paul Jones <shmiko@gmail.com>
2015-03-25 07:39:13 +1100

    removed app.use cors

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ require('./server/config/passport')();
 
 require('./server/config/routes')(app);
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://elite-schedule.azure-mobile.net/tables/leagues");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://elite-schedule.azure-mobile.net/tables/leagues");
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+//   next();
+// });
 
 app.listen(config.port);
 console.log('Listening on port ' + config.port + '...');
