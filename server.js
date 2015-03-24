cbb9378231722f2047287e81f77b5b64658f28fa
Paul Jones <shmiko@gmail.com>
2015-03-24 22:24:00 +1100

    multivision repackaged 2c

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://visiongo.heroku.com");
+  res.header("Access-Control-Allow-Origin", "https://elite-schedule.azure-mobile.net/tables/leagues");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
