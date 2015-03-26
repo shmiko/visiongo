c14aa299f2c22f02a003367c006c941339251063
Paul Jones <shmiko@gmail.com>
2015-03-27 08:15:10 +1100

    .

--- public/app/customers/controllers/customers.client.controller.js
+++ public/app/customers/controllers/customers.client.controller.js
@@ -19,7 +19,6 @@ tripstompApp.controller('CustomersController', ['$scope', '$stateParams', '$loca
 			    templateUrl: 'app/customers/views/create-customer.client.view.html',
 			    controller: function($scope, $modalInstance){
 			    	
-
 			    	$scope.ok = function () {
 			    		//if (createCustomerForm.$valid) {
 							$modalInstance.close();
